ee6fc6610158211eacb7627005dab581880e9c24
Jagger Bean <jagger@utexas.edu>
2022-05-07 12:26:16 -0500

    updating worker and jobs

--- worker.py
+++ worker.py
@@ -1,14 +1,7 @@
-from jobs import q
+from jobs import q, update_job_status
 
-
-  @<...>   # fill in
-  def execute_job(jid):
-      """
-      Retrieve a job id from the task queue and execute the job.
-      Monitors the job to completion and updates the database accordingly.
-      """
-      # fill in ...
-      # the basic steps are:
-      # 1) get job id from message and update job status to indicate that the job has started
-      # 2) start the analysis job and monitor it to completion.
-      # 3) update the job status to indicate that the job has finished.
+@q.worker
+def execute_job(jid):
+    jobs.update_job_status(jid, 'in progress')
+    time.sleep(15)
+    jobs.update_job_status(jid, 'complete')

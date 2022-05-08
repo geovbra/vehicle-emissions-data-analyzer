ac556861ac81776023d9c562b57199ec326161c8
George Braun <98420002+geovbra@users.noreply.github.com>
2022-05-08 12:31:47 -0500

    Src (#3)
    * adding the base of api, jobs and worker files
    
    * changing flask_api.py to api.py
    
    * Delete flask_api.py
    
    * updating api
    
    * updating worker and jobs
    
    * adding src folder
    
    * remove stuff
    
    * added first read function
    
    Co-authored-by: Jagger Bean <jagger@utexas.edu>
    Co-authored-by: JaggerBean <97973832+JaggerBean@users.noreply.github.com>

---
+++ src/api.py
@@ -0,0 +1,97 @@
+import json
+from flask import Flask, request
+import jobs
+import csv
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def how_to():
+
+    """
+    Shows how to use the app to get the results you want.
+    Returns: 
+        string: all of the possible inputs that the server is looking for.
+    """
+
+
+    logging.debug('used GET to access "how_to"')
+
+    return 'test'
+
+
+@app.route('/reset', methods=['POST'])
+def read_data_from_file_into_dict():
+    
+    """
+    Used to read in the data for later use with other functions. (Updates the list of data dictionaries)
+    Returns:
+        string: informing the user that the data is now available for use.
+    """
+
+    data = {}
+    data['vehicle_emissions'] = []
+    
+    logging.debug('used POST to read in data')
+
+    with open('uk_gov_data_dense_preproc.csv' , 'r', encoding = "ISO-8859-1") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data['vehicle_emissions'].append(dict(row))
+
+    rd.set('vehicle_emissions', json.dumps(data))
+
+    return f'Data has been read from file\n'
+
+# now emissions_data will be accessible to other functions
+
+@app.route('/read', methods=['GET'])
+def read_how_to():
+
+    """
+    Shows how to use the read route to get the desired data output.
+    Returns: 
+        string: all of the possible inputs that the server is looking for.
+    """
+
+
+    logging.debug('used GET to access "how_to"')
+
+    return 'test_read_how_to'
+
+
+@app.route('/read/<string:key>', methods=['GET'])
+def read(key:str):
+    logging.debug('used GET to access specific data specified by a key')
+
+    temp_data = []
+    
+    key = f'{key}'
+
+    if key == "all"
+        for row in data:
+            temp_data.append(row)
+    
+    return jsonify(temp_data)
+
+   
+
+
+@app.route('/jobs', methods=['POST'])
+def jobs_api():
+    """
+    API route for creating a new job to do some analysis. This route accepts a JSON payload
+    describing the job to be created.
+    """
+
+
+    try:
+        job = request.get_json(force=True)
+    except Exception as e:
+        return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+    return json.dumps(jobs.add_job(job['start'], job['end']))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

97d13f21691e4a57b51528fd4ecf1eb74cdbf946
George Braun <98420002+geovbra@users.noreply.github.com>
2022-05-09 16:15:06 -0500

    Api (#12)
    * adding the base of api, jobs and worker files
    
    * changing flask_api.py to api.py
    
    * Delete flask_api.py
    
    * updating api
    
    * updating worker and jobs
    
    * adding src folder
    
    * remove stuff
    
    * added first read function
    
    * adding two more read routes
    
    * added colons
    
    * added redis to api
    
    * fixing jobs to have redis
    
    * adding two more read routes
    
    * removed logs
    
    * fix
    
    * fix post
    
    * sss
    
    * add jsonify
    
    * add jsonify
    
    * remove folder
    
    * fix flask
    
    * fix read
    
    * fix read
    
    * fix read
    
    * fixing stuff
    
    * added delete routes to api
    
    * fix emissions spelling
    
    * fix emissions spelling
    
    * fix emissions spelling
    
    * fix emissions spelling
    
    * fix emissions spelling
    
    * fix emissions spelling
    
    * added json input for update route
    
    * fix error checking in read
    
    * added JSON input and exception handling for create route
    
    * fix error checking in read
    
    * fix error checking in read
    
    * fix error checking in read
    
    * fix error checking in read
    
    * fix error checking in read
    
    Co-authored-by: Jagger Bean <jagger@utexas.edu>
    Co-authored-by: JaggerBean <97973832+JaggerBean@users.noreply.github.com>

--- src/api.py
+++ src/api.py
@@ -76,102 +76,183 @@ def read(key:str):
     key = f'{key}'
 
     if key == "all":
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             temp_data.append(row)
     
     elif key == "car_id":
         start = request.args.get('start')
-	
-        #if type(start) != int:
-        #    return 'please use an integer as your query parameter'
-
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['car_id'] == start:
                 temp_data.append(row)
 
     elif key == "manufacturer":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['manufacturer'] == start:
                 temp_data.append(row)
 
     elif key == "model":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['model'] == start:
                 temp_data.append(row)
 
     elif key == "description":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['description'] == start:
                 temp_data.append(row)
   
     elif key == "transmission":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['transmission'] == start:
                 temp_data.append(row)
 
     elif key == "transmission_type":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['transmission_type'] == start:
                 temp_data.append(row)
 
     elif key == "engine_size_cm3":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['engine_size_cm3'] == start:
                 temp_data.append(row)
 
     elif key == "fuel":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['fuel'] == start:
                 temp_data.append(row)
 
     elif key == "powertrain":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['powertrain'] == start:
                 temp_data.append(row)
 
     elif key == "power_ps":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['power_ps'] == start:
                 temp_data.append(row)
 
     elif key == "co2_emissions_gPERkm":
         start = request.args.get('start')
-        if type(start) != str:
-            return 'please use a string as your query parameter'
-        for row in rd.get('vehicle_emissions'):
+        for row in json.loads(rd.get('vehicle_emissions'))['vehicle_emissions']:
             if row['co2_emissions_gPERkm'] == start:
                 temp_data.append(row)
 
     return jsonify(temp_data)
 
-   
+
+@app.route('/update/<string:ID>', methods=['POST'])
+def update_data(ID:str):
+
+    ID = int(float(ID))
+
+    temp_data = json.loads(rd.get('vehicle_emissions'))
+
+    try:
+        params = request.get_json(force=True)
+    except Exception as e:
+        return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+
+    try:
+        field = params['field']
+        value = params['value']
+    except Exception as e:
+        return "Please provide a JSON dictionary with keys \"field\" and \"value\""
+
+    if field == 'car_id':
+        return 'You cannot alter the car_id!'
+    if field != 'manufacturer' and field != 'model' and field != 'description' and field != 'transmission' and field != 'trasnmission_type' and field != 'engine_size_cm3' and field != 'fuel' and field != 'powertrain' and field != 'power_ps' and field != 'co2_emissions_gPERkm':
+        return 'please enter a valid string for field.'
+    updated_entry = {}  
+ 
+    for row in temp_data['vehicle_emissions']:
+        if int(float(row['car_id'])) == ID:
+            row[field] = value
+            updated_entry = row
+
+    rd.set('vehicle_emissions', json.dumps(temp_data))
+
+    return jsonify(updated_entry)
+
+@app.route('/create/new_entry', methods=['POST'])
+def create_data():
+    
+    temp_data = json.loads(rd.get('vehicle_emissions'))
+
+    try:
+        params = request.get_json(force=True)
+    except Exception as e:
+        return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+    
+    try:
+        manufacturer = params['manufacturer']
+        model = params['model']
+        description = params['description']
+        transmission = params['transmission']
+        transmission_type = params['transmission_type']
+        engine_size_cm3 = params['engine_size_cm3']
+        fuel = params['fuel']
+        powertrain = params['powertrain']
+        power_ps = params['power_ps']
+        co2_emissions_gPERkm = params['co2_emissions_gPERkm']
+    except Exception as e:
+        return "Please provide a JSON dictionary containing values for all fields (except for car_id)"
+
+    length = len(temp_data['vehicle_emissions'])
+    last_val = int(float(temp_data['vehicle_emissions'][length-1]['car_id']))
+
+    data_new = {}
+
+    data_new['car_id'] = str(float(last_val + 1))
+    data_new['manufacturer'] = manufacturer
+    data_new['model'] = model
+    data_new['description'] = description
+    data_new['transmission'] = transmission
+    data_new['transmission_type'] = transmission_type
+    data_new['engine_size_cm3'] = engine_size_cm3
+    data_new['fuel'] = fuel
+    data_new['powertrain'] = powertrain
+    data_new['power_ps'] = power_ps
+    data_new['co2_emissions_gPERkm'] = co2_emissions_gPERkm
+
+    temp_data['vehicle_emissions'].append(data_new)
+
+    rd.set('vehicle_emissions', json.dumps(temp_data))
+    return jsonify(data_new)
+
+@app.route('/delete', methods=['GET'])
+def delete_how_to():
+    """
+    Shows how to use the delete route to get the desired data output.
+    Returns:
+        string: all of the possible inputs that the server is looking for.
+    """
+
+@app.route("/delete/<string:field>", methods=['POST'])
+def delete(field:str):
+
+    temp_data = json.loads(rd.get('vehicle_emissions'))
+    new_data = {}
+    new_data['vehicle_emissions'] = []
+    value = request.args.get('value')
+
+    for row in temp_data['vehicle_emissions']:
+        if row[field] != value:
+            new_data['vehicle_emissions'].append(dict(row))
+
+    rd.set('vehicle_emissions', json.dumps(new_data))
+
+    if temp_data == new_data:
+        return "no data entries contain the field-value pair entered"
+
+    return "data successfully deleted"
 
 
 @app.route('/jobs', methods=['POST'])
@@ -180,12 +261,10 @@ def jobs_api():
     API route for creating a new job to do some analysis. This route accepts a JSON payload
     describing the job to be created.
     """
-
-
     try:
         job = request.get_json(force=True)
     except Exception as e:
-        return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+        return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
     return json.dumps(jobs.add_job(job['start'], job['end']))
 
 if __name__ == '__main__':

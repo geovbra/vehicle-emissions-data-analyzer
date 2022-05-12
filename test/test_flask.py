9571464b9f00770d785d50a94a4ef710790a81d2
Jagger Bean <jagger@utexas.edu>
2022-05-11 23:29:30 -0500

    updating test

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,5 +1,8 @@
-import pytest
+import pytest, requests
 
+def test_flask():
+    response = requests.get('https://isp-proxy.tacc.utexas.edu/geovbra-2/')
+    assert response.status_code == 200
 
 if __name__ == "__main__":
     pytest.main()
